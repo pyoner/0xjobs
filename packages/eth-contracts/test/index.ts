cd4931099390e39c50bfedff31f64eb587b9e3ff
Askar Yusupov <devex.soft@gmail.com>
2022-06-17 14:14:07 +0500

    test(eth-contracts): add check JobPosted event

--- packages/eth-contracts/test/index.ts
+++ packages/eth-contracts/test/index.ts
@@ -1,19 +1,12 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("JobPosts", function () {
+  it("Should post a job", async function () {
+    const JobPosts = await ethers.getContractFactory("JobPosts");
+    const jobPosts = await JobPosts.deploy();
+    await jobPosts.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await jobPosts.postJob("<cid>")).to.emit(jobPosts, "JobPosted");
   });
 });
