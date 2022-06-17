f37eb0787240267000eb4b2474c01dfa942c6cff
Askar Yusupov <devex.soft@gmail.com>
2022-06-17 19:51:21 +0500

    test(eth-contract): fix tests

--- packages/eth-contracts/test/index.ts
+++ packages/eth-contracts/test/index.ts
@@ -1,21 +1,31 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { JobPosts } from "../typechain";
 
 describe("JobPosts", function () {
   let contract: JobPosts;
+  let signers: SignerWithAddress[];
+  let defaultSigner: SignerWithAddress;
 
   before(async function () {
+    signers = await ethers.getSigners();
+    defaultSigner = signers[0];
     const Contract = await ethers.getContractFactory("JobPosts");
     contract = await Contract.deploy();
     await contract.deployed();
   });
 
   it("#postJob should post a job", async function () {
-    expect(await contract.postJob("<cid>")).to.emit(contract, "JobPosted");
+    await expect(contract.postJob("<cid>"))
+      .to.emit(contract, "JobPosted")
+      .withArgs(defaultSigner.address, 0);
   });
 
   it("#removeJob should remove a job", async function () {
-    expect(await contract.removeJob(0)).to.emit(contract, "JobRemoved");
+    const id = 0;
+    await expect(contract.removeJob(id))
+      .to.emit(contract, "JobRemoved")
+      .withArgs(defaultSigner.address, id);
   });
 });
